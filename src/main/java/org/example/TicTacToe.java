dd5468266013e4ca6f1b2c6699484285c61decd2
KrAk2007 <krakshit2006@gmail.com>
2026-05-03 17:06:35 +0530

    UC10

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -5,37 +5,25 @@ public class TicTacToe {
     public static void main(String[] args) {
 
         char[][] board = {
-                {'X', 'X', 'X'},
-                {'O', '-', 'O'},
-                {'-', '-', '-'}
+                {'X', 'O', 'X'},
+                {'X', 'O', 'O'},
+                {'O', 'X', 'X'}
         };
 
-        char winner = '-';
+        boolean isDraw = true;
 
         for (int i = 0; i < 3; i++) {
-            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                winner = board[i][0];
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    isDraw = false;
+                }
             }
         }
 
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] != '-' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
-                winner = board[0][j];
-            }
-        }
-
-        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            winner = board[0][0];
-        }
-
-        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-            winner = board[0][2];
-        }
-
-        if (winner != '-') {
-            System.out.println("Winner: " + winner);
+        if (isDraw) {
+            System.out.println("Draw");
         } else {
-            System.out.println("No winner");
+            System.out.println("Moves remaining");
         }
     }
 }
