f60bc08ad8ca9ece50a2955e84ca901766111055
KrAk2007 <krakshit2006@gmail.com>
2026-05-03 16:49:16 +0530

    UC6

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -4,26 +4,30 @@ public class TicTacToe {
 
     public static void main(String[] args) {
 
-        char[][] board = {
-                {'-', '-', '-'},
-                {'-', '-', '-'},
-                {'-', '-', '-'}
-        };
+        char[][] board = new char[3][3];
 
-        int row = 1;
-        int col = 1;
-
-        boolean valid = isValidMove(board, row, col);
-
-        System.out.println("Valid Move: " + valid);
-    }
-
-    public static boolean isValidMove(char[][] board, int row, int col) {
-
-        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
-            return true;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
         }
 
-        return false;
+        int row = 2;
+        int col = 0;
+        char symbol = 'O';
+
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+            board[row][col] = symbol;
+            System.out.println("Move successful");
+        } else {
+            System.out.println("Invalid move");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
